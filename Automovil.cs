22775848f762d972ace1e0560e5c0d5ec0dcca4d
faemv <faemv@FABIAN>
2023-10-15 19:42:07 -0600

    Agregar archivos de proyecto.

---
+++ Automovil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace BiblioCarro
+{
+    public class Automovil : Carro
+    {
+        private string placas;
+        public string Placas
+        {
+            get => placas;
+            set
+            {
+                if (placas == null)
+                {
+                    placas = "Placas en Trámite";
+                }
+                else
+                {
+                    placas = value;
+                }
+            }
+        }
+        public void Placa()
+        {
+            Console.WriteLine("Ingrese las placas del Carro a añadir:");
+            Placas = Console.ReadLine();
+        }
+        public override void MostrarDatos()
+        {
+            Console.WriteLine("\nCarro añadido con exito\nMarca: {0}\nModelo: {1}\nPlacas: {2}\nNúmero de Pasajeros: {3}\nPrecio: ${4}\n ", Marca, Modelo, Placas, Pasajeros, Precio);
+            Console.ReadLine();
+        }
+        public Automovil()
+        {
+            Saludar();
+            Marcas();
+            LimpiarDisplay();
+            Modelos();
+            LimpiarDisplay();
+            Placa();
+            LimpiarDisplay();
+            NumPasajeros();
+            LimpiarDisplay();
+            Precios();
+            LimpiarDisplay();
+            MostrarDatos();
+
+        }
+
+    }
+}
---
+++ Carro.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace BiblioCarro
+{
+    public abstract class Carro
+    {
+        #region Atributos
+        private string marca;
+        private int modelo;
+        private float precio;
+        private byte pasajeros;
+
+
+        public string Marca
+        { get => marca; set => marca = value; }
+        public int Modelo { get => modelo; set => modelo = value; }
+        public float Precio { get => precio; set => precio = value; }
+        public byte Pasajeros { get => pasajeros; set => pasajeros = value; }
+        #endregion
+
+        #region Constructor
+        public Carro()
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Precio = precio;
+            Pasajeros = Pasajeros;
+        }
+        #endregion
+
+        #region Metodos
+        public void Saludar()
+        {
+            Console.WriteLine("Bienvenido a la Agencia FI\n");
+        }
+        public void Marcas()
+        {
+            Console.WriteLine("Ingrese la marca del Carro a añadir:");
+            Marca = Console.ReadLine();
+        }
+        public void Modelos()
+        {
+            Console.WriteLine("Ingrese el modelo del Carro a añadir:");
+            Modelo = int.Parse(Console.ReadLine());
+        }
+       
+        public void NumPasajeros()
+        {
+            Console.WriteLine("Ingrese el numero de pasajeros que admite el Carro:");
+            Pasajeros = byte.Parse(Console.ReadLine());
+        }
+        public void Precios()
+        {
+            Console.WriteLine("Ingrese el precio del Carro a añadir:");
+            Precio = float.Parse(Console.ReadLine());
+        }
+        public virtual void MostrarDatos()
+        {
+            Console.WriteLine("\nCarro añadido con exito\nMarca: {0}\nModelo: {1}\nNúmero de Pasajeros: {2}\nPrecio: ${3}\n", Marca, Modelo, Pasajeros, Precio);
+            Console.ReadLine();
+        }
+
+        public void LimpiarDisplay()
+        {
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+        #endregion
+    }
+}
